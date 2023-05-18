90e08a403a5f4bdcf654d5118748382acf88b0dd
satyamk4 <karalkarsatyam@gmail.com>
2023-05-18 19:43:35 +0530

    new operations are added

--- calc.py
+++ calc.py
@@ -1,5 +1,14 @@
+#addition operation
 def myAdd(num1,num2):
     return num1+num2
 
-result = myAdd(20,30)
-print(result)
+#sub operation
+def mySub(num1,num2):
+    return num1-num2
+
+
+result1 = myAdd(20,30)
+result2 = mySub(60,20)
+
+print(result1)
+print(result2)
