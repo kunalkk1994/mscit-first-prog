5fd0cc5f841a27257ef06b054514b53323658102
satyamk4 <karalkarsatyam@gmail.com>
2023-05-18 19:47:52 +0530

    two different functions are added

--- calc.py
+++ calc.py
@@ -6,9 +6,20 @@ def myAdd(num1,num2):
 def mySub(num1,num2):
     return num1-num2
 
+#mul operation
+def myMul(num1,num2):
+    return num1*num2
+
+#div operation
+def myDiv(num1,num2):
+    return num1/num2
 
 result1 = myAdd(20,30)
 result2 = mySub(60,20)
+result3 = myMul(2,6)
+result4 = myDiv(10,2)
 
 print(result1)
 print(result2)
+print(result3)
+print(result4)
